9373038bc73a93a6fc782c5b32d09647fe7849a4
josh-bearss-codes <josh.bearss@gmail.com>
2026-03-01 15:42:05 -0700

    Project 1: hello.py - input, variables, f-strings - requires int for age

---
+++ hello.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+year_born = 2026 - age
+
+print(f'Hello {name}, since you are {age} years old, you were born in {year_born}')
